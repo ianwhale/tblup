2767e27b5bc7d97c0bba2055b8d2d2045eedbe03
Ian <ianwhale@github.com>
2018-11-03 22:21:41 -0400

    Update experiment generation script.

--- generate_sbs.py
+++ generate_sbs.py
@@ -12,7 +12,7 @@ base_str = """#!/bin/bash
 #SBATCH --time=04:00:00          
 #SBATCH --ntasks=40                  
 #SBATCH --nodes=1 
-#SBATCH --mem=128G
+#SBATCH --mem=186G
 #SBATCH --job-name={name}
 #SBATCH --array=0-9
 #SBATCH --output={output}
@@ -35,7 +35,6 @@ def write_sb(name, extras):
     Write to a SLURM batch file.
     :param name: string, name of run and file.
     :param extras: list, joined with " " and put into {extras}.
-    :return:
     """
     features = 1000 if "randkeys" in name else 100
 
@@ -65,21 +64,28 @@ regressors = ["intercv_blup", "intracv_blup", "montecv_blup"]
 strategies = ["de_currenttobest_1", "sade", "mde_pbx"]
 seeders = ["top_snps"]
 conditions = ["h2_max", "h2_min", "h2_median", "h2_mean"]
-alphas = [0, 0.1, 0.2]
+alphas = [0, 0.1, 0.2, -0.05, -0.1, -0.2]
 r_vals = [None, 500, 250]
 individuals = ["randkeys", "coevolve"]
-gammas = [0.875, 0.75]
+gammas = [0.875, 0.75, 0.625, 0.5, 0.375, 0.25]
+best_gamma = 0.75
 
+# General Experiments.
 for indiv in individuals:
     extras = [individual.format(indiv)]
 
     # Nothing special job.
     write_sb(indiv, extras)
 
+    # Gamma tuning experiments.
     if indiv == "coevolve":
         for gamma in gammas:
             write_sb("_".join([indiv, "g_{}".format(str(gamma).replace(".", "_"))]),
                      extras + [coevolve_gamma.format(gamma)])
+
+    # Set best gamma.
+    if indiv == "coevolve":
+        extras += [coevolve_gamma.format(best_gamma)]
 
     # Different cross-validation schemes.
     for r in regressors:
@@ -107,3 +113,30 @@ for indiv in individuals:
         else:
             write_sb("_".join([indiv, "remove_r_all_a_{}".format(string_a)]),
                      extras + [remove_snps, h2_alpha.format(a)])
+
+best_dict = {
+    regressor: "montecv_blup",
+    h2_alpha: 0,
+}
+
+# Pairwise Combination Experiments.
+for indiv in individuals:
+    name_as_list = [indiv]
+    extras = [individual.format(indiv)]
+
+    if indiv == "coevolve":
+        extras += [coevolve_gamma.format(best_gamma)]
+
+    extras += [regressor.format(best_dict[regressor])]
+    name_as_list += [best_dict[regressor]]
+
+    # Monte + self-adaptive
+    for s in strategies:
+        write_sb("_".join(name_as_list + [s]), extras + [de_strategy.format(s)])
+
+    extras += [h2_alpha.format(best_dict[h2_alpha])]
+    name_as_list += ["h2_min_" + str(best_dict[h2_alpha]).replace(".", "_")]
+
+    # Monte + self-adaptive + alpha = 0
+    for s in strategies:
+        write_sb("_".join(name_as_list + [s]), extras + [de_strategy.format(s)])
